c5dac036396079e10098c2b4b0d6610a17713259
0.kgy <my@kaguya.org>
2024-12-01 21:21:56 +0800

    tweak prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+# git filters.
 git config --local include.path ../.gitconfig
+
+# local git hooks.
 git config --local core.hooksPath .githooks/
 
+# add cargo installed programs to path.
 if [[ -z $(grep "$HOME/.cargo/bin" <<< $PATH) ]]; then
-    echo "# added by ./prepare.sh\nPATH=~/.cargo/bin:\$PATH" >> ~/.bash_profile
+    printf "# added by ./prepare.sh\nPATH=~/.cargo/bin:\$PATH" \
+	     >> ~/.bash_profile
     PATH=~/.cargo/bin:$PATH
 fi
 
-rustup component add rust-analyzer rustfmt clippy
-command -v sccache >&- || cargo install sccache --locked
-command -v taplo >&- || cargo install taplo-cli --locked
+# add these components.
+for component in rust-analyzer rustfmt clippy; do
+    rustup component list --installed | grep -q $component ||
+	rustup component add $component
+done
+
+# use sccache for optimized compile time & space usage.
+command -v sccache >&- ||
+    cargo install sccache --locked
+
+# use taplo for toml formatting.
+command -v taplo >&- ||
+    cargo install taplo-cli --locked
